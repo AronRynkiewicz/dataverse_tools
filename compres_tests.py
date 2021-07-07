ed10df7fd5d462dc5246150cfc4f9fdbf4b9e69a
Aron <aron.rynkiewicz@gmail.com>
2021-07-07 11:23:03 +0200

    Update compres_tests.py

--- compres_tests.py
+++ compres_tests.py
@@ -1,17 +1,25 @@
 import unittest
 import shutil
+import sys
 from compres import *
 
 # python -m unittest compres_tests.py
 
 NUMBER_OF_FILES = 3500
 DUMMY_DIR_NAME = 'folder1'
+UNZIPPED_DIR_NAME = 'folder_unzip'
 DATASET_NAME = '16S_D31-cyt180j_1'
 TEST_ZIP_NAME = 'test'
 
+PATH_DICT = {
+    'win32': '\\',
+    'linux': '/',
+    'darwin': '/',
+}
 
-def create_dummy_dir():
-        os.mkdir(DUMMY_DIR_NAME)
+
+def create_dummy_dir(dir_name):
+    os.mkdir(dir_name)
 
 
 def create_dummy_files(number_files):
@@ -35,35 +43,43 @@ def create_dummy_files(number_files):
     os.chdir('..')
     return lst
 
+
 def unzipFile(lst_files):
     for i in lst_files:
-        with ZipFile(i, 'r') as zipObj:
+        with ZipFile('..' + PATH_DICT[sys.platform] + i, 'r') as zipObj:
             zipObj.extractall()
 
+
 class TestCompres(unittest.TestCase):
+    files_lst = []
+
 
     def setUp(self):
-        create_dummy_dir()
+        create_dummy_dir(DUMMY_DIR_NAME)
+        create_dummy_dir(UNZIPPED_DIR_NAME)
         create_dummy_files(NUMBER_OF_FILES)
-    
+        self.files_lst = checkZip(DUMMY_DIR_NAME, TEST_ZIP_NAME, DATASET_NAME)
+
+
     def test_zip_creation(self):
-        checkZip(DUMMY_DIR_NAME, TEST_ZIP_NAME, DATASET_NAME)
         dirs_count = len([file for file in os.listdir() if file.startswith(TEST_ZIP_NAME) and file.endswith('.zip')])
         self.assertEqual(math.ceil(NUMBER_OF_FILES / LIMIT), dirs_count)
 
+
     def test_filenames(self):
-        lst_files=checkZip(DUMMY_DIR_NAME, TEST_ZIP_NAME, DATASET_NAME)
-        unzipFile(lst_files)
+        os.chdir(UNZIPPED_DIR_NAME)
+        unzipFile(self.files_lst)
         file_count = len([file for file in os.listdir() if file.startswith(DATASET_NAME) and file.endswith('.h5')])
         self.assertEqual(NUMBER_OF_FILES, file_count)
+        os.chdir('..')
+
 
     def tearDown(self):
         shutil.rmtree(DUMMY_DIR_NAME)
+        shutil.rmtree(UNZIPPED_DIR_NAME)
 
         for i in range(math.ceil(NUMBER_OF_FILES / LIMIT)):
             os.remove(TEST_ZIP_NAME + str(i) + '.zip')
-        for i in range(NUMBER_OF_FILES):
-            os.remove(DATASET_NAME + '_data_' + str(i) + '.h5')
 
 
 if __name__ == '__main__':
