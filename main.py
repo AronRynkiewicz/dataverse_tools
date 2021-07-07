33be8fd28e2aa03ff66432f9b7933a860e9617ee
Aron <aron.rynkiewicz@gmail.com>
2021-07-07 13:54:10 +0200

    Update main.py
    - added zip description as param

--- main.py
+++ main.py
@@ -28,12 +28,23 @@ parser.add_argument(
         help = 'Common prefix for all files to be uploaded. Notice that script expects that file id will be separated by _ from common prefix, eg.: file_0001.txt file_0002.txt',
     )
 
+parser.add_argument(
+        '-msg',
+        '--message',
+        type=str,
+        required=True,
+        help = 'File with description for zip files.',
+    )
+
 args = parser.parse_args()
+
+ZIP_FILES_DESCRIPTION = open(args.message).read()
 
 print('Creating new dataset with metadata from: ' +
         args.json_file +
         ', files from: ' + args.dir +
-        ' dricractory, with common prefix: ' + args.files_prefix
+        ' dricractory, with common prefix: ' + args.files_prefix +
+        ' and this text as desricption of all send files: ' + ZIP_FILES_DESCRIPTION
     )
 
 api = NativeApi(MXRDR_PATH, API_TOKEN)
@@ -53,7 +64,7 @@ if connection_code == 200:
     print('Sending zips to dataset...')
     for it, file in enumerate(zip_files_list):
         print('Uploading file #' + str(it))
-        code = upload_file_to_dataset(data['data']['persistentId'], file, 'ZIP file')
+        code = upload_file_to_dataset(data['data']['persistentId'], file, ZIP_FILES_DESCRIPTION)
         print('Status: ' + str(code))
     print('Done')
 
