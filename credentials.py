981e9917c652bc45e37a2d5036ee068dbe83a44c
Aron <aron.rynkiewicz@gmail.com>
2021-07-12 11:22:07 +0200

    added credentials template

---
+++ credentials.py
@@ -0,0 +1,22 @@
+
+API = {
+    'LOGIN_DATA' : {
+        'username': '',
+        'password': '',
+    },
+
+    'API_TOKEN': '',
+}
+
+SELENIUM = {
+    'LOGIN_DATA' : {
+        'username': '',
+        'password': '',
+    },
+
+
+    'SITE_LOGIN_DATA' : {
+        'username': '',
+        'password': '',
+    },
+}
